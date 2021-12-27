41f21aa706b8b12052ba6dd2698ad3817e83aef3
Stefan <stefan.gabriel.muscalu@gmail.com>
2021-12-27 21:25:35 +0200

    fix: login text

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -84,49 +84,41 @@ export const Auth = observer(function Auth() {
           >
             Login
           </Button>
+
+          <Paper elevation={2} className={classes.info}>
+            <Typography variant="h6" marginBottom="16px">
+              This tool allows you to easily view your different weapon types from your inventory and vault.
+            </Typography>
+
+            <Typography>
+              You have to log in to enable this tool to grab your weapons from the API.
+            </Typography>
+
+            <Typography>
+              If you found a bug or have a feature request, please open an issue in the <a
+              href="https://github.com/legraphista/d2-weapons-breakdown">Github Repository</a>!
+            </Typography>
+
+            <Typography>
+              You can also find me on twitter{' '}
+              <a href="https://twitter.com/legraphista">
+                <Twitter fontSize="inherit"/> @legraphista
+              </a>
+            </Typography>
+
+            <Typography style={{marginTop: 16}}>
+              <b>D2 Weapons breakdown</b> uses the Bungie.net api and it's OAuth authorization.
+            </Typography>
+            <Typography>
+              You log in on an official Bungie.net page and <b>D2 Weapons breakdown</b> receives a temporary login token
+              from Bungie.
+            </Typography>
+            <Typography>
+              That means it does not receive your credentials.
+            </Typography>
+          </Paper>
         </>
       )}
-
-      <Paper elevation={2} className={classes.info}>
-        <Typography variant="h6" marginBottom="16px">
-          This tool allows you to easily view your different weapon types from your inventory and vault.
-        </Typography>
-
-        <Typography>
-          You have to log in to enable this tool to grab your weapons from the API.
-        </Typography>
-
-        <Typography>
-          If you found a bug or have a feature request, please open an issue in the <a
-          href="https://github.com/legraphista/d2-weapons-breakdown">Github Repository</a>!
-        </Typography>
-
-        <Typography>
-          You can also find me on twitter{' '}
-          <a href="https://twitter.com/legraphista">
-            <Twitter fontSize="inherit"/> @legraphista
-          </a>
-        </Typography>
-
-        <Typography style={{marginTop: 16}}>
-          <b>D2 Weapons breakdown</b> uses the Bungie.net api and it's OAuth authorization.
-        </Typography>
-        <Typography>
-          You log in on an official Bungie.net page and <b>D2 Weapons breakdown</b> receives a temporary login token
-          from Bungie.
-        </Typography>
-        <Typography>
-          That means it does not receive your credentials.
-        </Typography>
-      </Paper>
-
-      {/*This tool allows you to select the optimal armor from your inventory and vault to satisfy your stat needs. It also tells you which stat mods you have to use!
-
-You have to log in to enable this tool to grab your armor from the API.
-If you like my tools and want to support me, head to ko-fi.com and buy me a coffee! ❤
-If you found a bug or have a feature request, please open an issue in the Github Repository!
-
-D2ArmorPicker uses the Bungie.net api and it's OAuth authorization. You log in on an official Bungie.net page and D2ArmorPicker receives a temporary login token from Bungie. That means it does not receive your credentials.*/}
 
       {loading && (
         <Loading/>
